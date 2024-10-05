234b20037a3e2ac877c5ab09e2a21e6e029a8087
pixelo1 <hoan1015@gmail.com>
2024-10-05 19:47:15 +0900

    init

---
+++ src/main/java/com/pixelo/health/wellplatefoodnutrientbatch/WellPlateFoodNutrientBatchApplication.java
@@ -0,0 +1,13 @@
+package com.pixelo.health.wellplatefoodnutrientbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellPlateFoodNutrientBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WellPlateFoodNutrientBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pixelo/health/wellplatefoodnutrientbatch/WellPlateFoodNutrientBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixelo.health.wellplatefoodnutrientbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellPlateFoodNutrientBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
